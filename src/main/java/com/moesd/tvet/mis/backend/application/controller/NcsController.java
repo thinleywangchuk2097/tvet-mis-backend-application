219811393db5013619302ca0c0b5c853bbe322d7
Thinlleey Wangchuk <thinleywangchuk2097@gmail.com>
2026-06-17 10:56:02 +0600

    latest code

--- src/main/java/com/moesd/tvet/mis/backend/application/controller/NcsController.java
+++ src/main/java/com/moesd/tvet/mis/backend/application/controller/NcsController.java
@@ -1,17 +1,13 @@
 package com.moesd.tvet.mis.backend.application.controller;
 
 import java.util.List;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.moesd.tvet.mis.backend.application.dto.CourseEnrollmentAppdto;
 import com.moesd.tvet.mis.backend.application.dto.Ncsdto;
 import com.moesd.tvet.mis.backend.application.service.NcsService;
 import lombok.RequiredArgsConstructor;
