6ea5c63ee8a66ec8a05c2d3a85aee00451e7c8e7
DorjiWangdi <dorjiwangdi90@gmail.com>
2026-07-03 15:32:57 +0600

    nac

---
+++ src/main/java/com/moesd/tvet/mis/backend/application/dto/NcsUnitDto.java
@@ -0,0 +1,16 @@
+package com.moesd.tvet.mis.backend.application.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class NcsUnitDto {
+    private Integer unitId;
+    private String unitCode;
+    private String unitTitle;
+}
--- src/main/java/com/moesd/tvet/mis/backend/application/model/NcsApp.java
+++ src/main/java/com/moesd/tvet/mis/backend/application/model/NcsApp.java
@@ -6,37 +6,44 @@ import java.util.List;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 
 @Data
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-
 @Table(name = "tbl_ncs_curriculum_publication")
 public class NcsApp {
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)  // For MySQL/PostgreSQL auto-increment
-	@Column(name = "publication_id", nullable = false, unique = true)
-	private Integer publicationId;
-	private Integer occupationId;
-	private Integer certificationId;
-	private String courseTitle;
-	private String ncsCode;
-	private Date validityDate;
-	private String publicationType;
-	private Integer createdBy;
-	private Integer updatedBy;
-
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "publication_id", nullable = false, unique = true)
+    private Integer publicationId;
+    
+    private Integer occupationId;
+    private Integer certificationId;
+    private String courseTitle;
+    
+    @Temporal(TemporalType.DATE)
+    @Column(name = "validity_date")
+    private Date validityDate;
+    
+    private String publicationType;
+    private Integer createdBy;
+    private Integer updatedBy;
+    
+    @OneToMany(mappedBy = "ncsApp", cascade = CascadeType.ALL, fetch = FetchType.LAZY, orphanRemoval = true)
+    private List<NcsUnit> units;
 }
